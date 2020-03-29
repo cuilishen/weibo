61b1b5d3388bc474f576f6ea6f3e900156b1b683
vagrant <vagrant@homestead>
2020-03-29 07:44:27 +0000

    登录成功后导航逻辑

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -6,9 +6,13 @@
   <body>
     @include('layouts._header')
     <div class="container">
-      @include('shared._messages')
+      <div class="offset-md-1 col-md-10">
+        @include('shared._messages')
       @yield('content')
       @include('layouts._footer')
+      </div>
+
     </div>
+    <script src="{{ mix('js/app.js') }}"></script>
   </body>
 </html>
