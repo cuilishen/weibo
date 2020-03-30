0ded8d21dc0bac98f5d41953c7d4f8537b8909cb
cuilishen <cuilishen@live.cn>
2020-03-30 10:48:10 +0000

    change data of user

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -37,5 +37,29 @@ class UsersController extends Controller
         return redirect()->route('users.show',[$user]);
     }
 
+    public function edit(User $user)
+    {
+        return view('users.edit',compact('user'));
+    }
+
+    public function update(User $user,Request $request)
+    {
+        $this->validate($request,[
+            'name' => 'required|max:50',
+            'password' => 'required|confirmed|min:6'
+        ]);
+
+        $data = [];
+        $data['name'] = $request->name;
+        if ($request->password) {
+                $data['password']=bcrypt($request->password);
+        }
+        $user->update($data);
+
+        session()->flash('success','Person data was updated!');
+
+        return redirect()->route('users.show',$user->id);
+    }
+
 
 }
