427be3bdecc237ea1c7bb8d94b0eb95f171e0782
cuilishen <cuilishen@live.cn>
2020-03-31 11:50:02 +0000

    删除微博

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -3,6 +3,7 @@
 
 @section('content')
 <div class="row">
+  users.show.blade.php
   <div class="offset-md-2 col-md-8">
     <section class="user_info">
       @include('shared._user_info', ['user' => $user])
