16b8d441d094e45a71b6e269a2bebf4c806b7354
cuilishen <cuilishen@live.cn>
2020-03-31 02:59:54 +0000

    查看用户列表cls

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -11,7 +11,7 @@ class UsersController extends Controller
     public function __construct()
     {
         $this->middleware('auth',[
-            'except' => ['show','create','store']
+            'except' => ['show','create','store','index']
         ]);
 
         $this->middleware('guest',[
@@ -73,5 +73,12 @@ class UsersController extends Controller
         return redirect()->route('users.show',$user->id);
     }
 
+
+    public function index()
+    {
+        //$users = User::all();
+        $users = User::paginate(5);
+        return view('users.index',compact('users'));
+    }
 
 }
