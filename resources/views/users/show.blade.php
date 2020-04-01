db601b47296c4606c6932cda3353e725a2df8978
cuilishen <cuilishen@live.cn>
2020-04-01 07:44:02 +0000

    统计社交信息

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -3,10 +3,12 @@
 
 @section('content')
 <div class="row">
-  users.show.blade.php
   <div class="offset-md-2 col-md-8">
     <section class="user_info">
       @include('shared._user_info', ['user' => $user])
+    </section>
+    <section class="stats mt-2">
+      @include('shared._stats', ['user' => $user])
     </section>
     <section class="status">
       @if ($statuses->count() > 0)
